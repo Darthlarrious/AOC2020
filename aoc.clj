dd7840d228591e7366b90cca25f18a42b843a6f1
Laurent Gratton <laurent.gratton@gmail.com>
2020-12-02 23:38:26 -0600

    Day 3

--- aoc.clj
+++ aoc.clj
@@ -1,6 +1,7 @@
 (ns aoc
   (:require [days.day1 :as d1]
-            [days.day2 :as d2]))
+            [days.day2 :as d2]
+            [days.day3 :as d3]))
 
 (defn runDays []
   (print "Day 1 Part 1:" (d1/day1Part1 2020 d1/day1Input) "\n")
@@ -8,6 +9,9 @@
   (print "------\n")
   (print "Day 2 Part 1:" (d2/day2Part1) "\n")
   (print "Day 2 Part 2:" (d2/day2Part2) "\n")
+  (print "------\n")
+  (print "Day 3 Part 1:" (d3/day3Part1) "\n")
+  (print "Day 3 Part 2:" (d3/day3Part2) "\n")
   )
 
 (defn -main [] (runDays))
---
+++ days/day3.clj
@@ -0,0 +1,34 @@
+(ns days.day3
+  (:require [clojure.string :as s]))
+
+(def day3Input (s/split-lines (slurp "inputs/day3.txt")))
+(defn isTreeAt [map x y]
+  (if (= \# (nth (nth map (mod y (count map))) (mod x (count (nth map 0)))))
+    1
+    0))
+(defn countTrees [map]
+  (let [targetY (count map)]
+    (loop [currX 0 currY 0 treeCount 0]
+      (if (> currY targetY)
+        treeCount
+        (recur (+ 3 currX) (+ 1 currY) (+ (isTreeAt map currX currY) treeCount)))
+      )))
+
+(defn countTreesAlgo [map incX incY]
+  (let [targetY (count map)]
+    (loop [currX 0 currY 0 treeCount 0]
+      (if (> (inc currY) targetY)
+        treeCount
+        (recur (+ incX currX) (+ incY currY) (+ (isTreeAt map currX currY) treeCount)))
+      )))
+
+(def addAlgos
+  (*
+    (countTreesAlgo day3Input 1 1)
+    (countTreesAlgo day3Input 3 1)
+    (countTreesAlgo day3Input 5 1)
+    (countTreesAlgo day3Input 7 1)
+    (countTreesAlgo day3Input 1 2)))
+
+(defn day3Part1 [] (countTrees day3Input))
+(defn day3Part2 [] addAlgos)
