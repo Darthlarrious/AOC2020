feb4657cdabae9fe26e6b7d6a7d672032db3f4e0
Laurent Gratton <laurent.gratton@gmail.com>
2020-12-04 12:32:05 -0600

    Day 4 Part 2

--- days/day4.clj
+++ days/day4.clj
@@ -26,6 +26,54 @@
             fields)))))
   )
 
+(defn validateHeight [elem]
+  (or
+    (and
+      (> (count elem) 2)
+      (= '(\i \n) (take-last 2 elem))
+      (let [heightStr (subs elem 0 (- (.length elem) 2)) height (try (Integer/parseInt heightStr) (catch NumberFormatException e 0))]
+        (and
+          (>= height 59)
+          (<= height 76))))
+    (and
+      (> (count elem) 2)
+      (= '(\c \m) (take-last 2 elem))
+      (let [heightStr (subs elem 0 (- (.length elem) 2)) height (try (Integer/parseInt heightStr) (catch NumberFormatException e 0))]
+        (and
+          (>= height 150)
+          (<= height 193))))))
+
+(def validators {
+                  :byr (fn [elem] (and (= 4 (count elem)) (try (Integer/parseInt elem) (catch NumberFormatException e false)) (>= (Integer/parseInt elem) 1920) (<= (Integer/parseInt elem) 2020)))
+                  :iyr (fn [elem] (and (= 4 (count elem)) (try (Integer/parseInt elem) (catch NumberFormatException e false)) (>= (Integer/parseInt elem) 2010) (<= (Integer/parseInt elem) 2020)))
+                  :eyr (fn [elem] (and (= 4 (count elem)) (try (Integer/parseInt elem) (catch NumberFormatException e false)) (>= (Integer/parseInt elem) 2020) (<= (Integer/parseInt elem) 2030)))
+                  :hgt (fn [elem] (validateHeight elem))
+                  :hcl (fn [elem] (re-matches #"^#[a-fA-F0-9]{6}$" elem))
+                  :ecl (fn [elem] (in? '["amb" "blu" "brn" "gry" "grn" "hzl" "oth"] elem))
+                  :pid (fn [elem] (= 9 (count elem)))
+                  :cid (fn [_] true)
+                  })
+
+(defn validateField [fieldData validators]
+  (let [field (s/split fieldData #":")
+        fieldName (first field)
+        fieldData (last field)]
+    (try (((keyword fieldName) validators) fieldData) (catch NullPointerException e false))
+   ))
+
+
+(defn isValidPassport2? [passport keys validators]
+  (let [fields (s/split passport #" ")]
+    (and
+      (not
+        (>
+          (count keys)
+          (count
+            (filter
+              #(in? keys (getKey %))
+              fields))))
+      (reduce #(and %1 %2) (map #(validateField % validators) fields))))
+  )
 
 (defn day4Part1 [] (count (filter #(isValidPassport? % passportKeys) passports)))
-(defn day4Part2 [] nil)
+(defn day4Part2 [] (count (filter #(isValidPassport2? % passportKeys validators) passports)))
