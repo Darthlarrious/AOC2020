e73fd8f610b359caf30f7bfb786b7ac989ad7d2c
Laurent Gratton <laurent.gratton@gmail.com>
2020-12-10 12:03:05 -0600

    Day 10 P1 - fixed

--- days/day10.clj
+++ days/day10.clj
@@ -2,10 +2,10 @@
   (:require [clojure.string :as s]))
 
 
-(def day10Input (sort (map #(Integer/parseInt %) (s/split-lines (slurp "inputs/day10.txt")))))
+(def day10Input (conj (sort (map #(Integer/parseInt %) (s/split-lines (slurp "inputs/day10.txt")))) 0))
 
 (def test (map #(- (nth day10Input (inc %)) (nth day10Input %)) (range (dec (count day10Input)))))
 (def gaps (frequencies (map #(- (nth day10Input (inc %)) (nth day10Input %)) (range (dec (count day10Input))))))
 
-(defn day10Part1 [] (* (gaps 1) (gaps 3)))
+(defn day10Part1 [] (* (gaps 1) (inc (gaps 3))))
 (defn day10Part2 [] nil)
