a81fd0ff12bee0db1c2f4f62b523f10dbf787fa3
Laurent Gratton <laurent.gratton@gmail.com>
2020-12-10 15:28:21 -0600

    Day 10 P2 - Inspired by tampix77 on reddit

--- days/day10.clj
+++ days/day10.clj
@@ -1,11 +1,30 @@
 (ns days.day10
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.math.combinatorics :as m]
+            [clojure.core.reducers :as reducers]))
 
 
 (def day10Input (conj (sort (map #(Integer/parseInt %) (s/split-lines (slurp "inputs/day10.txt")))) 0))
 
-(def test (map #(- (nth day10Input (inc %)) (nth day10Input %)) (range (dec (count day10Input)))))
-(def gaps (frequencies (map #(- (nth day10Input (inc %)) (nth day10Input %)) (range (dec (count day10Input))))))
+(def gaps (map #(- (nth day10Input (inc %)) (nth day10Input %)) (range (dec (count day10Input)))))
 
-(defn day10Part1 [] (* (gaps 1) (inc (gaps 3))))
-(defn day10Part2 [] nil)
+(def deviceVoltage (+ 3 (apply max day10Input)))
+
+(def day10Input2 (sort (conj day10Input deviceVoltage)))
+
+(defn getNextOnes [list start]
+  (filter #(and (<= (- % start) 3) (> (- % start) 0 )) list))
+
+(defn day10
+  [voltages]
+    (let [ways (reduce #(assoc %1 %2 (->> (range (- %2 3) (inc %2))
+                                          (keep %1)
+                                          (reduce +)))
+                       {(first voltages) 1}
+                       (rest voltages))]
+      (ways (last voltages))))
+
+
+(defn day10Part1 [] (let [nums (frequencies gaps)]
+                      (* (nums 1) (inc (nums 3)))))
+(defn day10Part2 [] (day10 day10Input2))
